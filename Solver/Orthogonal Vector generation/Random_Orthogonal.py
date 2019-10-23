c4a4341184cb8404e2105008e7365010a0d21321
Abdul Hannan Faruqi <abdul.hannan@zhcet.ac.in>
2019-10-23 17:31:24 +0530

    Add files via upload

---
+++ Solver/Orthogonal Vector generation/Random_Orthogonal.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 14:38:35 2019
+
+@author: Abdul Hannan
+"""
+
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+size = 3
+v = []
+v1 = np.random.rand(size)
+v1n = v1/np.linalg.norm(v1)
+v.append(v1n)
+
+i=1
+
+while (i<size):
+    v0 = np.random.rand(size)
+    
+    for vector in v:
+        v0 = v0 - ((np.dot(v0, vector)/np.dot(vector, vector))*np.array(vector))
+    
+    vn = v0/np.linalg.norm(v0)
+    v.append(list(vn))
+    i+=1
+
+v= np.array(v)
+plv = np.ndarray(shape = (3,6))        
+for i in range (len(v)):
+    plv[i] = list(np.zeros(size))+ list(v[i])
+X,Y,Z,P,Q,R = zip(*plv) 
+   
+if __name__ == "__main__":
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+#    ax.plot3D(v[:,0], v[:,1], v[:,2], 'k.', alpha = 0.8)
+    ax.quiver(X,Y,Z,P,Q,R)
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([-1, 1])
+    plt.show()
