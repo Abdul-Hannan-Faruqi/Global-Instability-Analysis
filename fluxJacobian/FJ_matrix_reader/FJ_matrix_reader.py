2a370d1613ddf8bb700bee1df9fe7447feb3eb5e
Ahmad Faraz Badar <61835815+Ahmad-Faraz-Badar@users.noreply.github.com>
2020-03-21 19:21:18 +0530

    Added flux jacobian csv reader

---
+++ fluxJacobian/FJ_matrix_reader/FJ_matrix_reader.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 16:54:49 2020
+
+@author: Ahmad Faraz
+"""
+
+import numpy as np
+
+filename = 'FJ_matrix.csv'
+f = open(filename, 'r')
+
+n = input("Enter size of matrix: ")
+n = int(n)
+M = np.zeros((n, n))
+
+s = f.readline() #reads header
+s = f.readline()
+while(s): #while the line read from the file is not empty
+    u = s.split(',') #splitting into a list 
+    u[0] = int(float(u[0])) #conversion of i_index
+    u[1] = int(float(u[1])) #conversion of j_index
+    u[2] = float(u[2].rstrip('\n')) #conversion of value at i,j
+    M[u[0]][u[1]] = u[2]
+    s = f.readline()
+    
+f.close()
