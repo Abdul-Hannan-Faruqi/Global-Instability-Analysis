a4b4183fcea7f5b0a9ea4c5f3108a371fb9ebef9
Abdul Hannan Faruqi <abdul.hannan@zhcet.ac.in>
2020-04-05 16:20:12 +0530

    Update SIM.f90

--- Solver/Fortran/SIM.f90
+++ Solver/Fortran/SIM.f90
@@ -5,19 +5,19 @@ module data_mod
     implicit none
     SAVE
     complex*16 :: alpha = (1.0,0.0), beta = (0.0,0.0)
-    integer:: n, m=20
+    integer:: n, m=10
     complex*16::shift_par
 end module
 
 Program Eigenvalue
 USE data_mod
 implicit none
-integer::i, j, K, ok, row, col, iter, ierr, x
-character(len=50):: e_str, filename
-complex*16, dimension(:,:), allocatable:: A, B, Th, evec, R, Ax, Bx, C, Phi
+integer::i, j, K, ok, row, col, iter, ierr, ierr2
+character(len=50):: e_str, filename, e_str2
+complex*16, dimension(:,:), allocatable:: A, B, Th, evec, Ax, Bx, C, Phi, Int_arr
 complex*16, dimension(:), allocatable:: eval, WORK1, work2, eigs, tau
 double precision, dimension(:), allocatable:: w, w2
-integer, dimension(:), allocatable::IPIV
+integer, dimension(:), allocatable::IPIV, IPIV2
 integer, dimension(:,:), allocatable::eye
 complex*16 DUMMY(1,1)
 real(8) :: val, tol= 0.00000001, e = 1.0
@@ -36,7 +36,6 @@ print *, n
 
 allocate(A(n,n))
 allocate(B(n,n))
-allocate(R(n,m))
 allocate(Th(n,m))
 allocate(Ax(m,m))
 allocate(Bx(m,m))
@@ -49,8 +48,10 @@ allocate(eval(m))
 allocate(eigs(m))
 allocate(tau(m))
 allocate(IPIV(m))
+allocate(IPIV2(n))
 allocate(evec(m,m))
 allocate(Phi(n,m))
+allocate(Int_arr(m,n))
 allocate(eye(n,n))
 
 eigs=0.0
@@ -67,12 +68,14 @@ enddo
 close(100)
 print *, "File closed"
 
+open(unit=200, file= "Eigs.dat", status = 'new', action = 'write', iostat = ierr2, iomsg = e_str2)
+
 do i=1,n
     do j=1, m
         if (i.eq.j) then
-            R(i,j) = 1
+            Th(i,j) = 1
         else
-            R(i,j)= 0
+            Th(i,j)= 0
         end if
     end do
 end do
@@ -92,33 +95,46 @@ iter = 0
 call cpu_time(start_time)
 do
     iter = iter+1
-!   Multiplication
-    call zgemm('N', 'N', n, m, n, alpha, A, n, R, n, beta, Th, n)
+!   Pre-multiplication
+    call zgesv(n, m, A, n, ipiv2, Th, n, ok)
+
+!   Normalize Th
     Th_norm = zlange("F", n, m, Th, n, w)
     Th = Th/Th_norm
+
 !   QR decomposition
     call zgeqrf(n, m, Th, n, tau, work1, m*m, ok)
     call zungqr(n, m, m, Th, n, tau, work1, m*m, ok)
 !   Project A and B onto the subspace spanned by Th
-    call Project(A, Th, Ax)
-    call Project(B, Th, Bx)
+
+!    call Project(A, Th, Ax)   | Replace subroutine to prevent 
+!    call Project(B, Th, Bx)   | Stack Overflow
+    call zgemm('T', 'N', m, n, n, alpha, Th, n, A, n, beta, Int_arr, m)
+    call zgemm('N', 'N', m, m, n, alpha, Int_arr, m, Th, n, beta, Ax, m)
+    call zgemm('T', 'N', m, n, n, alpha, Th, n, B, n, beta, Int_arr, m)
+    call zgemm('N', 'N', m, m, n, alpha, Int_arr, m, Th, n, beta, Bx, m)
+
 !   Calculating inv(Bx)
     call zgetrf(m, m, Bx, m, IPIV, ok)
     call zgetri(m, Bx, m, IPIV, work1, m*m, ok)
+
 !   C = inv(Bx)*Ax
     call zgemm('N', 'N', m, m, m, alpha, Bx, m, Ax, m, beta, C, m)
+
 !   Eigenvalues and right eigenvectors of C
     call ZGEEV('N', 'V', m, C, m, eval, DUMMY, 1, evec, m, WORK2, 2*m, W2, ok)
-!   Calculate new R
+
+!   Calculate new Th
     call zgemm('N', 'N', n, m, m, alpha, Th, n, evec, m, beta, Phi, n)
-    call zgemm('N', 'N', n, m, n, alpha, B, n, Phi, n, beta, R, n)
+    call zgemm('N', 'N', n, m, n, alpha, B, n, Phi, n, beta, Th, n)
+
     e = abs(maxval(real(eval)-real(eigs)))
     eigs = eval
-    if (iter.eq.50) then
+    if (iter.eq.500) then
         shift_par = minval(real(eigs))
         A = A - shift_par*eye
     end if
-    if ((e.lt.tol)) then
+    if ((e.lt.tol).or.(iter.eq.4000)) then
         exit
     end if
 end do
@@ -131,6 +147,10 @@ print *, "error = ", e
 print *, "Shift = ", shift_par
 eigs = eigs + shift_par
 print *, "Eigenvalues = ", eigs
+
+do i=1,m
+    write(200, *) 1/eigs(i)
+enddo
 
 end program
 
